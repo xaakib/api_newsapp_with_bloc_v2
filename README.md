8eb2a3450ae0478fcbd5c88dc9be52688216bef2
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-02-07 00:22:41 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# api_newsapp_with_bloc_v2
+ 
---
+++ android/app/src/main/kotlin/com/example/api_newsapp_with_bloc_v2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.api_newsapp_with_bloc_v2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
