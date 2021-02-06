f46b8f203ff20e46543e239bca7c20a1558eceb1
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-02-07 02:19:52 +0600

    gethotnews bloc are done

--- lib/bloc/get_hotnews_bloc.dart
+++ lib/bloc/get_hotnews_bloc.dart
@@ -6,4 +6,17 @@ class GetHotNewsBloc {
   final NewsRepository _repository = NewsRepository();
   final BehaviorSubject<ArticleResponse> _subject =
       BehaviorSubject<ArticleResponse>();
+
+  getHotNews() async {
+    ArticleResponse response = await _repository.getHotNews();
+    _subject.sink.add(response);
+  }
+
+  dispose() {
+    _subject.close();
+  }
+
+  BehaviorSubject<ArticleResponse> get subject => _subject;
 }
+
+final getHotNewsBloc = GetHotNewsBloc();
