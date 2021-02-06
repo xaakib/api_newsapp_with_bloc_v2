f0c3247bbabcd89bba8b364e2136e53a607cb5f7
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-02-07 02:15:30 +0600

    bug fixed BehaviorSubject

--- lib/bloc/get_hotnews_bloc.dart
+++ lib/bloc/get_hotnews_bloc.dart
@@ -1,14 +1,9 @@
 import 'package:api_newsapp_with_bloc_v2/model/article_response.dart';
 import 'package:api_newsapp_with_bloc_v2/repository/repository.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:rxdart/rxdart.dart';
 
 class GetHotNewsBloc {
   final NewsRepository _repository = NewsRepository();
-
   final BehaviorSubject<ArticleResponse> _subject =
-
-  // getSourceNews(String sourceId) async {
-  //   ArticleResponse response = await _repository.getSourceNews(sourceId);
-  //   _subject.sink()
-  // }
+      BehaviorSubject<ArticleResponse>();
 }
