453d1f452105f1fb9904405e64d3044494cda0a5
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-02-07 03:20:37 +0600

    home and bottom navigatin done

--- lib/screens/mian_screen.dart
+++ lib/screens/mian_screen.dart
@@ -1,6 +1,8 @@
 import 'package:api_newsapp_with_bloc_v2/bloc/bottom_navBar_bloc.dart';
+import 'package:api_newsapp_with_bloc_v2/model/article.dart';
 import 'package:flutter/material.dart';
 import 'package:api_newsapp_with_bloc_v2/style/theme.dart' as theme;
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 
 class MainScreen extends StatefulWidget {
   @override
@@ -9,6 +11,7 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   BottomNavBarBloc _bottomNavBarBloc;
+  ArticleModel _articleModel;
 
   @override
   void initState() {
@@ -18,6 +21,7 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
+    print(_articleModel.title);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: PreferredSize(
@@ -26,6 +30,89 @@ class _MainScreenState extends State<MainScreen> {
           backgroundColor: theme.Colors.mainColor,
           title: Text("NewsApp"),
         ),
+      ),
+      body: SafeArea(
+        child: StreamBuilder<NavBarItem>(
+          stream: _bottomNavBarBloc.itemStream,
+          initialData: _bottomNavBarBloc.defaultItem,
+          builder: (BuildContext context, AsyncSnapshot<NavBarItem> snapshot) {
+            switch (snapshot.data) {
+              case NavBarItem.HOME:
+                return Container();
+
+              case NavBarItem.SOURCES:
+                return Container();
+
+              case NavBarItem.SEARCH:
+                return Container();
+            }
+          },
+        ),
+      ),
+      bottomNavigationBar: StreamBuilder(
+        stream: _bottomNavBarBloc.itemStream,
+        initialData: _bottomNavBarBloc.defaultItem,
+        builder: (BuildContext context, AsyncSnapshot<NavBarItem> snapshot) {
+          return Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(30),
+                  topLeft: Radius.circular(30),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey[100],
+                      spreadRadius: 0,
+                      blurRadius: 10.0)
+                ]),
+            child: ClipRRect(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30),
+                topRight: Radius.circular(30),
+              ),
+              child: BottomNavigationBar(
+                backgroundColor: Colors.white,
+                iconSize: 20.0,
+                unselectedItemColor: Colors.grey,
+                unselectedFontSize: 9.5,
+                selectedFontSize: 9.5,
+                type: BottomNavigationBarType.fixed,
+                fixedColor: theme.Colors.mainColor,
+                currentIndex: snapshot.data.index,
+                onTap: _bottomNavBarBloc.picItem,
+                items: [
+                  BottomNavigationBarItem(
+                    // ignore: deprecated_member_use
+                    title: Padding(
+                      padding: const EdgeInsets.only(top: 5.0),
+                      child: Text("Home"),
+                    ),
+                    icon: Icon(EvaIcons.homeOutline),
+                    activeIcon: Icon(EvaIcons.home),
+                  ),
+                  BottomNavigationBarItem(
+                    // ignore: deprecated_member_use
+                    title: Padding(
+                      padding: const EdgeInsets.only(top: 5.0),
+                      child: Text("Source"),
+                    ),
+                    icon: Icon(EvaIcons.gridOutline),
+                    activeIcon: Icon(EvaIcons.home),
+                  ),
+                  BottomNavigationBarItem(
+                    // ignore: deprecated_member_use
+                    title: Padding(
+                      padding: const EdgeInsets.only(top: 5.0),
+                      child: Text("Search"),
+                    ),
+                    icon: Icon(EvaIcons.searchOutline),
+                    activeIcon: Icon(EvaIcons.home),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
